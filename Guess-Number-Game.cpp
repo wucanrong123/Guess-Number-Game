24a0fe59c7782b754f4d67cd7b6154cc9ec85148
wucanrong123 <18965672727@163.com>
2025-04-18 21:20:18 +0800

    Add files via upload

---
+++ Guess-Number-Game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+int L, R;
+void print(string str) {
+	for (int i = 0; i < str.size(); i++) {
+		putchar(str[i]);
+		Sleep(20);
+	}
+}
+int main() {
+	print("²ÂÊýÓÎÏ·1.0\n");
+	scanf("%d%d", &L, &R);
+	return 0;
+} 
